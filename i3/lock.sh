26f8af30999d63877af4e0dc95132a6e4c813715
Kaleb Klein <klein.jae@gmail.com>
2017-03-16 20:39:03 -0400

    Added lock screen script for i3lock
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

---
+++ i3/lock.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# take current screen snapshot
+scrot /tmp/screen.png
+
+# define color and get screen size
+COLOR=#373d48
+SIZE=$(xrandr --current | grep '*' | uniq | awk '{print $1}')
+
+# generate new image from size and color
+convert -size ${SIZE} xc:${COLOR} /tmp/screen-color.png
+
+# change alpha channel of solid color image
+convert /tmp/screen-color.png -alpha set -channel A -evaluate set 50% /tmp/screen-color.png
+
+# paint and blur the original scrot image
+convert /tmp/screen.png -paint 3 -blur 0x5 /tmp/screen.png
+
+# composite translucent solid color image to the original scrot image
+convert /tmp/screen.png /tmp/screen-color.png -gravity center -composite -matte /tmp/screen.png
+
+# composite our lock icon onto the generated scrot image
+convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+
+# run i3lock and display with the new generated image
+i3lock -e -i /tmp/screen.png
